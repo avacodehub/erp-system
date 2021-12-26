8196872fe43a8e9c53c385eda6a2e633688736b7
avacodehub <avacodehub@gmail.com>
2021-12-26 12:15:46 +0300

    Add test

---
+++ erp-system/Model/Detail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace erp_system.Model
+{
+    public class Detail
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("number")]
+        public int Number { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("revision")]
+        public int Revision { get; set; }
+
+        [BsonElement("drawingNum")]
+        public string DrawingNum { get; set; }
+
+        [BsonElement("drawingRevision")]
+        public int DrawingRevision { get; set; }
+        public Nullable<bool> IsAssembly { get; set; }
+        public Nullable<bool> IsOwn { get; set; }
+        public string[] Bom { get; set; }
+
+
+    }
+}
