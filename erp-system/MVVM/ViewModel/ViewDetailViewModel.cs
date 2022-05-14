d11e06386a51ae12c1b92b0316f5a4c7ae44fab5
Serj <avacodehub@gmail.com>
2022-05-14 15:54:40 +0300

    add view detail page (#10)

---
+++ erp-system/MVVM/ViewModel/ViewDetailViewModel.cs
@@ -0,0 +1,36 @@
+﻿using erp_system.MVVM.Model;
+using erp_system.Stores;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace erp_system.MVVM.ViewModel
+{
+    public class ViewDetailViewModel : ObservableObject
+    {
+        public ICommand EditCommand { get; set; }
+        public ICommand CreateNewCommand { get; set; }
+
+        private DetailFull _detailFull;
+
+        public DetailFull DetailFull
+        {
+            get => _detailFull;
+            set => SetProperty(ref _detailFull, value);
+        }
+
+        public ViewDetailViewModel(DetailFull detailFull, Action onCreateNew, Action<DetailFull?> onEdit)
+        {
+            DetailFull = detailFull;
+            EditCommand = new RelayCommand<DetailFull>(onEdit);
+            CreateNewCommand = new RelayCommand(onCreateNew);
+        }
+
+       
+    }
+}
